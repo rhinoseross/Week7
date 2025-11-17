89cdf7a3ca9ffe7bfd127af62765e80720f9a257
Rhinoseross <rossleonce+git@gmail.com>
2025-11-17 14:38:46 +0000

    Update contact.php
    Change name to test

--- mySite/contact.php
+++ mySite/contact.php
@@ -10,8 +10,8 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h2>Contact me</h2>
-	<p>"Contact Us" conten goes here.  I'll stick with teh "lorem ipsum" as well, so that the footer isn't immediately following this text.</p>
+	<h2>Contact Ross</h2>
+	<p>"Contact Ross" conten goes here.  I'll stick with teh "lorem ipsum" as well, so that the footer isn't immediately following this text.</p>
 
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
